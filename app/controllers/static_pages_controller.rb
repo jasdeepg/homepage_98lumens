7834fbf5a8bca102aecd18df021b92a244f5f137
Jasdeep Garcha <jasdeepsgarcha@gmail.com>
2012-09-26 11:03:21 -0700

    initial

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def dashboard
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homepage98lumens::Application.config.secret_token = 'dc01dd52eb636f20e430d5275dc852ad6a464a776c6ca84fa0cf252f7dd541fb8eb5dc47eeec5c3e6ceaca7bffd6b832323d7a6d08cb78abae2d87d5c55196d5'
